e40a393d14771b6a7c290a80fcf9a364435ce398
Maximilian Piper <maximilianpiper@gmx.de>
2024-06-06 15:20:21 +0200

    step-3

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,27 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [num, setNum] = useState(0);
+  const [num2, setNum2] = useState(0);
+  const [score, setScore] = useState(0);
 
   function randomNumberInRange(min, max) {
     // 👇️ Get the number between min (inclusive) and max (inclusive)
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
-  const [num2, setNum2] = useState(0);
+  
   const handleClick = () => {
-    setNum(randomNumberInRange(1, 5));
-    setNum2(randomNumberInRange(1, 5));
+    setNum(randomNumberInRange(1, 6));
+    setNum2(randomNumberInRange(1, 6));
+    setScore(score+num+num2);
   };
   return (
     <div>
       <div>dice 1: {num}</div>
       <div>dice 2: {num2}</div>
+      <div>score= {score}</div>
       <button onClick={handleClick}>Roll dice</button>
     </div>
   );
