e0c96be6fda726997214ce6be0c0949447b8ac69
Maximilian Piper <maximilianpiper@gmx.de>
2024-06-06 15:58:04 +0200

    step-4

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,15 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [isButtonDisabled, setButtonDisabled] = useState(false);
+
+  const disableButton = () => {
+      setButtonDisabled(true);
+  };
+
+  const enableButton = () => {
+      setButtonDisabled(false);
+  };
   const [num, setNum] = useState(0);
   const [num2, setNum2] = useState(0);
   const [score, setScore] = useState(0);
@@ -16,13 +25,25 @@ function App() {
     setNum(randomNumberInRange(1, 6));
     setNum2(randomNumberInRange(1, 6));
     setScore(score+num+num2);
+    if(score>=100) {
+      disableButton();
+    }
+    else {
+      enableButton();
+    };
+    if(score==100){
+      alert("Congratulations, you won!");
+    };
+    if(score>100){
+      alert("You lose!");
+    };
   };
   return (
     <div>
       <div>dice 1: {num}</div>
       <div>dice 2: {num2}</div>
       <div>score= {score}</div>
-      <button onClick={handleClick}>Roll dice</button>
+      <button onClick={handleClick} disabled={isButtonDisabled}>Roll dice</button>
     </div>
   );
   
