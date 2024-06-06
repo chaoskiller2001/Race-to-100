228b48dd29d3806e7bdb3605ba92e6eeb07b52d1
Maximilian Piper <maximilianpiper@gmx.de>
2024-06-06 12:25:44 +0200

    OOF

--- src/App.jsx
+++ src/App.jsx
@@ -5,31 +5,34 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [num, setNum] = useState(0);
 
+  function randomNumberInRange(min, max) {
+    // 👇️ Get the number between min (inclusive) and max (inclusive)
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  const handleClick = () => {
+    setNum(randomNumberInRange(1, 5));
+  };
+  const [num2, setNum2] = useState(0);
+
+  function randomNumberInRange(min, max) {
+    // 👇️ Get the number between min (inclusive) and max (inclusive)
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  const handleClick2 = () => {
+    setNum2(randomNumberInRange(1, 5));
+  };
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h2>dice 1: {num}</h2>
+      <h3>dice 2: {num2}</h3>
+      <button onClick={() =>{handleClick(), handleClick2()}}>Roll dice</button>
+    </div>
+  );
+  
 }
 
 export default App
